ccab31c0e8f7fde60f343d9b4938c15bc5dd5f85
Bryson <bryson.rafael@gmail.com>
2016-11-06 13:08:29 -0600

    Remove second Kele

--- lib/kele.rb
+++ lib/kele.rb
@@ -1,4 +1,14 @@
-def initialize(username, password)
-  @username = username
-  @password = password
+require 'kele'
+
+class Client
+  include HTTParty
+
+  def initialize(email, password)
+    response = self.class.post(base_api_endpoint + "/sessions", body: {
+      email: email,
+      password: password
+    })
+
+    @auth_token = response["auth_token"]
+  end
 end
