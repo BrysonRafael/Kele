856937f7cde8cc3fcf5c5ce5158886286b1ade2c
Bryson <bryson.rafael@gmail.com>
2016-11-22 20:26:57 -0600

    Checkpoint 4: Complete

--- lib/kele.rb
+++ lib/kele.rb
@@ -22,4 +22,14 @@ class Kele
     )
     JSON.parse(response.body)
   end
+
+  def get_mentor_availability()
+    mentor_id = get_me["current_enrollment"]["mentor_id"]
+
+    response = self.class.get(
+      @base_api_endpoint + "/mentors/#{ mentor_id }/student_availability",
+      headers: { "authorization" => @auth_token }
+    )
+    JSON.parse(response.body)
+  end
 end
